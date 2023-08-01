64091159b2f3e6e1854f28c5d49b082375faa964
Arman Mokammel <armanmokammel@gmail.com>
2023-08-01 15:01:22 +0600

    Create Student.java

---
+++ LAB-02/Student.java
@@ -0,0 +1,30 @@
+
+public class Student {
+	private int id;
+	private float cgpa;
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public int getId()
+	{
+		return this.id;
+	}
+	
+	public void setCGPA(float cgpa)
+	{
+		this.cgpa = cgpa;
+	}
+	
+	public float getCGPA()
+	{
+		return this.cgpa;
+	}
+	
+	public void printRemarks()
+	{
+		System.out.print(id + " " + cgpa);
+	}
+}
