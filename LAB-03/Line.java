63f3d0b0d4ed0a1e7a2b42b77ed3986feb4501b5
Arman Mokammel <armanmokammel@gmail.com>
2023-08-06 01:43:37 -0700

    Create Line.java

---
+++ LAB-03/Line.java
@@ -0,0 +1,47 @@
+import java.lang.Math;
+
+public class Line {
+	private Point start;
+	private Point end;
+	
+	public Line(Point start, Point end)
+	{
+		this.start = start;
+		this.end = end;
+	}
+	
+	public Line(int x1, int y1, int x2, int y2)
+	{
+		start = new Point();
+		end = new Point();
+		start.setX(x1);
+		start.setY(y1);
+		end.setX(x2);
+		end.setY(y2);
+	}
+	
+	public Point getStart()
+	{
+		return this.start;
+	}
+	
+	public Point getEnd()
+	{
+		return this.end;
+	}
+	
+	public void setStart(Point start)
+	{
+		this.start = start;
+	}
+	
+	public void setEnd(Point end)
+	{
+		this.end = end;
+	}
+	
+	public double length()
+	{
+		return Math.sqrt(Math.pow((end.getX() - start.getX()), 2) + Math.pow((end.getY() - start.getY()), 2));
+	}
+}
