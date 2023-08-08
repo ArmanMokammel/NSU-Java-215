66e97a905c82d4ce6ee06d3383fb637344a221c6
Arman Mokammel <armanmokammel@gmail.com>
2023-08-08 12:42:15 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,27 @@ public class Main {
 		LinkedListCustom lk = new LinkedListCustom();
 		
 		lk.add("Holy");
-		lk.add("Shit");
+		lk.add("Moly");
+		lk.add("meow");
+		lk.add("Damn");
+		
+		System.out.println(lk);
+		
+		lk.remove("Moly");
+		
+		System.out.println(lk);
+		
+		lk.addFirst("Superman");
+		lk.addLast("WOW");
+		
+		System.out.println(lk);
+		
+		lk.add(2, "tweet");
+		
+		System.out.println(lk);
+		
+		lk.remove(3);
+
 		
 		System.out.println(lk);
 	}
