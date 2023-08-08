a179484005c2034a54814365444f0f1d2162d114
Arman Mokammel <armanmokammel@gmail.com>
2023-08-08 10:45:16 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String args[])
+	{
+		LinkedListCustom lk = new LinkedListCustom();
+		
+		lk.add("Holy");
+		lk.add("Shit");
+		
+		System.out.println(lk);
+	}
+}
