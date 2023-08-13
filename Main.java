e834d0546c1f51d6235a72fd6c70f9691a3ee807
Arman Mokammel <armanmokammel@gmail.com>
2023-08-13 10:25:11 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char[] string = {
+			'H','e','l','l','o',' ','W','o','r','l','d',
+		};
+		MyGhorarDim s = new MyGhorarDim(string);
+		
+		s.replace('e', 'u');
+		
+		System.out.println(s);
+	}
+
+}
