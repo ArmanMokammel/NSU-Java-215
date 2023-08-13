1e483e260ef88f1adb856edde84a68cb8b989dcf
Arman Mokammel <armanmokammel@gmail.com>
2023-08-14 00:29:43 +0600

    Update Main.java

--- LAB-05/Main.java
+++ LAB-05/Main.java
@@ -3,20 +3,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		char[] string = {
-				'H','e','l','l','o',' ','W','o','r','l','d'
-			};
-			MyString s = new MyString(string);
-			
-			System.out.println(s.length());
-			System.out.println(s);
-			s.replace('e', 'u');
-			System.out.println(s.charAt(4));
-			System.out.println(s.contains("Wor"));
-			System.out.println(s.equals("Hello World"));
-			System.out.println(s.indexOf('o'));
-			System.out.println(s.toUpperCase());
-			System.out.println(s.toLowerCase());
-			System.out.println(s.trim());
+				' ',' ',' ','H','e','l','l','o',' ','W','o','r','l','d',' ',' '
+			};		
+		
+		MyString s1 = new MyString(string);
+		MyString s2 = new MyString("   Hello World  ");
+		
+		s1.replace('e', 'u');
+		System.out.println(s1);
+		System.out.println(s1.length());
+		System.out.println(s1.charAt(4));
+		System.out.println(s1.contains("Wor"));
+		System.out.println(s1.equals("Hullo World"));		
+		System.out.println(s1.trim());
 
 	}
 
