9976b1b9d068aeb01ee2625085fbf6755e878e67
Arman Mokammel <armanmokammel@gmail.com>
2023-08-13 15:00:32 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		char[] string = {
-			'H','e','l','l','o',' ','W','o','r','l','d'
-		};
-		MyGhorarDim s = new MyGhorarDim(string);
-		
-		s.replace('e', 'u');
-		
-		System.out.println(s);
+				'H','e','l','l','o',' ','W','o','r','l','d'
+			};
+			MyString s = new MyString(string);
+			
+			System.out.println(s.length());
+			System.out.println(s);
+			s.replace('e', 'u');
+			System.out.println(s.charAt(4));
+			System.out.println(s.contains("Wor"));
+			System.out.println(s.equals("Hello World"));
+			System.out.println(s.indexOf('o'));
+			System.out.println(s.toUpperCase());
+			System.out.println(s.toLowerCase());
+			System.out.println(s.trim());
+
 	}
 
 }
