757e6c8f24a6b2a6f40a6f9257d74b8d335cf02d
Arman Mokammel <armanmokammel@gmail.com>
2023-08-20 13:18:16 +0600

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,19 @@
+
+public class Shape {
+	private String name;
+	
+	public Shape(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
---
+++ Square.java
@@ -0,0 +1,8 @@
+
+public class Square extends Rectangle{
+	
+	public Square(String name)
+	{
+		super(name);
+	}
+}
