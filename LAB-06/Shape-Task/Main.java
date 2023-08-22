1113706f7e8771dae578310959f0e60a7742520d
Arman Mokammel <armanmokammel@gmail.com>
2023-08-22 10:00:45 +0600

    Update Main.java

--- LAB-06/Shape-Task/Main.java
+++ LAB-06/Shape-Task/Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Square sq = new Square("My Square");
-		
-		sq.setSide1(5);
-		sq.setSide2(5);
+		Square sq = new Square(5, 5);
+		sq.setName("My Square");
 		
 		System.out.println("Area of " + sq.getName() + ": " + sq.area());
 		System.out.println("Perimeter of " + sq.getName() + ": " + sq.perimeter());
